e5cef75b088c919787ffda86812df0f10e4ce774
Torgeir Lebesbye <torgeir@lebesbye.com>
2021-06-27 17:32:28 +0200

    didn't realize the response was multilined (#16)

--- src/test_tradingpost.py
+++ src/test_tradingpost.py
@@ -9,4 +9,4 @@ class TestTradingpost(object):
     def test_flavor(self, testbot):
         testbot.push_message('!flavor lava axe (ulg)')
         result = testbot.pop_message()
-        assert(result == '"Catch!"')
+        assert(result == '"Catch!"\n(Lava Axe (ULG))')
